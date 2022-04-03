d84a910f739303970dcebd2b20e43429600473b9
amaharek <abdelrahman.mahareek@gmail.com>
2022-04-03 14:23:00 +0100

    adding two sum

---
+++ Solutions/LC0001.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hist = {}
+        for ind, num in enumerate(nums):
+            if target - num in hist: 
+                return [hist[target-num],ind]
+            hist[num] = ind
