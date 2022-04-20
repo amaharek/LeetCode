58afc109b91c18ecb3669a94d318d8cb1ee92dae
amaharek <abdelrahman.mahareek@gmail.com>
2022-04-20 16:39:14 +0100

    LC98

---
+++ Solutions/LC0098.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/validate-binary-search-tree/ 
+# Time complexity and Space O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import math 
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def validate(node,low=-math.inf, high = math.inf):
+            if not node: 
+                return True 
+            if node.val <=low or node.val >=high: 
+                return False
+            
+            return (validate(node.right,node.val,high) and validate(node.left,low,node.val)) 
+        return validate(root)
